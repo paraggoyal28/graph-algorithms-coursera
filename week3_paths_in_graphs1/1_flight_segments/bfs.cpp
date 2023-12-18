f43d213e37d37c918107f3adcb53546a4a86f290
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2023-12-18 21:30:06 +0530

    add week3 code solutions

--- week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-
-using std::vector;
+#define INF 1e9
 using std::queue;
+using std::vector;
 
-int distance(vector<vector<int> > &adj, int s, int t) {
-  //write your code here
+int distance(vector<vector<int>> &adj, int s, int t)
+{
+  // write your code here
+  size_t n = adj.size();
+  vector<int> dist(n, INF);
+  dist[s] = 0;
+  queue<int> bfs;
+  bfs.push(s);
+  while (!bfs.empty())
+  {
+    int node = bfs.front();
+    bfs.pop();
+
+    if (node == t)
+    {
+      return dist[node];
+    }
+
+    for (int neigh : adj[node])
+    {
+      if (dist[neigh] == INF)
+      {
+        dist[neigh] = dist[node] + 1;
+        bfs.push(neigh);
+      }
+    }
+  }
   return -1;
 }
 
-int main() {
+int main()
+{
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  for (int i = 0; i < m; i++)
+  {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -2,19 +2,55 @@
 #include <vector>
 #include <queue>
 
-using std::vector;
 using std::queue;
+using std::vector;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
+bool isBipartiteUtil(vector<vector<int>> &adj, int vertex, vector<int> &colors, int color)
+{
+  colors[vertex] = color;
+
+  for (int neigh : adj[vertex])
+  {
+    if (colors[neigh] == -1)
+    {
+      if (!isBipartiteUtil(adj, neigh, colors, 1 - color))
+      {
+        return false;
+      }
+    }
+    else if (colors[neigh] != 1 - color)
+    {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-int main() {
+int bipartite(vector<vector<int>> &adj)
+{
+  vector<int> colors(adj.size(), -1);
+
+  for (size_t vertex = 0; vertex < adj.size(); ++vertex)
+  {
+    if (colors[vertex] == -1)
+    {
+      if (!isBipartiteUtil(adj, vertex, colors, 0))
+      {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+int main()
+{
   int n, m;
   std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
+  vector<vector<int>> adj(n, vector<int>());
+  for (int i = 0; i < m; i++)
+  {
     int x, y;
     std::cin >> x >> y;
     adj[x - 1].push_back(y - 1);
